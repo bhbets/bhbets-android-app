804015b75127f730909e75db5bb6b85e15910337
Mateusz Urbański <matek2305@gmail.com>
2018-09-24 21:17:39 +0200

    temp remove date formatting on match list

--- app/src/main/java/com/example/murbanski/myapplication/MatchDataAdapter.java
+++ app/src/main/java/com/example/murbanski/myapplication/MatchDataAdapter.java
@@ -1,35 +1,20 @@
 package com.example.murbanski.myapplication;
 
 import android.content.Context;
-import android.graphics.Movie;
-import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class MatchDataAdapter extends ArrayAdapter<MatchData> {
 
     private Context mContext;
     private List<MatchData> matches;
-    private long diff, oldLong, newLong;
-
-    SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy HH:mm");
-    SimpleDateFormat formatter2 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
 
     public MatchDataAdapter(@NonNull Context context, List<MatchData> list) {
         super(context, 0 , list);
@@ -53,26 +38,16 @@ public class MatchDataAdapter extends ArrayAdapter<MatchData> {
 
         textViewHome.setText(currentMatch.getHomeTeam());
         textViewAway.setText(currentMatch.getAwayTeam());
-        textViewDate.setText(matchDate(currentMatch.getStartDate()));
+        textViewDate.setText(currentMatch.getStartDate());
 
-        if(currentMatch.getBet() != null){
-            textViewResultHome.setText(Integer.toString(currentMatch.getBet().getHomeTeam()));
-            textViewResultAway.setText(Integer.toString(currentMatch.getBet().getAwayTeam()));
-        }else {
+        if (currentMatch.getBet() != null) {
+            textViewResultHome.setText(String.valueOf(currentMatch.getBet().getHomeTeam()));
+            textViewResultAway.setText(String.valueOf(currentMatch.getBet().getAwayTeam()));
+        } else {
             textViewResultHome.setText("?");
             textViewResultAway.setText("?");
         }
 
         return listItem;
-    }
-
-    private String matchDate(String matchTime){
-        try {
-            Date dateMatch = formatter2.parse(matchTime);
-            return formatter.format(dateMatch);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 }
